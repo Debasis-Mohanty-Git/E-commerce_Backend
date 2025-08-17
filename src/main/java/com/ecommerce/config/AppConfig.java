ffde17fea996744089aed11666ddaf4cc4637a52
Debasis-Mohanty-Git <moghantydebasis976@gmail.com>
2025-08-17 10:52:19 +0530

    Final production Commit

--- src/main/java/com/ecommerce/config/AppConfig.java
+++ src/main/java/com/ecommerce/config/AppConfig.java
@@ -42,7 +42,8 @@ public class AppConfig {
             CorsConfiguration cfg = new CorsConfiguration();
             cfg.setAllowedOrigins(Arrays.asList(
                 "http://localhost:3000",
-                "http://localhost:4200"
+                "http://localhost:4200",
+                "https://e-commerce-frontend-three-sigma.vercel.app/"
             ));
             cfg.setAllowedMethods(Collections.singletonList("*"));
             cfg.setAllowCredentials(true);
